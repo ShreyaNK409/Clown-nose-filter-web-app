a4ba02cbb61f9d9f7d5bb82415e2900d553e0528
ShreyaNK409 <78596107+ShreyaNK409@users.noreply.github.com>
2021-06-17 15:28:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+nose_x=0;
+nose_y=0;
+
+
+function preload()
+{
+ reindeer_nose=loadImage("https://i.postimg.cc/7ZBcjDqp/clownnose.png") ;
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+  posenet = ml5.poseNet(video,modelLoaded);
+  posenet.on('pose',gotPoses);
+
+}
+
+function modelLoaded() {
+  console.log("model Loaded!");
+}
+
+function gotPoses(results) {
+  if (results.length > 0) {
+    console.log(results);
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y);
+    nose_x=results[0].pose.nose.x-10;
+    nose_y=results[0].pose.nose.y-10;
+  }
+}
+
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(reindeer_nose,nose_x,nose_y,20,20);
+}
+
+function takesnapshot() 
+{
+  save("myReindierSelfie.png");
+}
+
